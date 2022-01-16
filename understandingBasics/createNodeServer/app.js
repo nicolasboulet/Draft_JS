43101bb6ed15d56908511290e3007f4c9d8e9d10
Nicolas Boulet <nicolasboulet@live.fr>
2022-01-16 14:14:45 +0100

    routing request

--- understandingBasics/createNodeServer/app.js
+++ understandingBasics/createNodeServer/app.js
@@ -1,7 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  console.log(req)
+  // console.log(req.headers)
+  // console.log(req.method)
+  // console.log(req.url)
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body>');
+  res.write('<h1>Welcome from Node.js Server</h1>');
+  res.write('</body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
