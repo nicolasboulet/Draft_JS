a95d9040118c5fb7827138a15e74df11287ac035
Nicolas Boulet <nicolasboulet@live.fr>
2022-01-22 14:56:12 +0100

    repare som stuf

--- mvc/04-refactored-code/models/product.js
+++ mvc/04-refactored-code/models/product.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-  path.dirname(process.mainModule.filename),
+  path.dirname(require.main.filename),
   'data',
   'products.json'
 );
@@ -26,7 +26,7 @@ module.exports = class Product {
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
-        console.log(err);CDATASection.
+        console.log(err);
       });
     });
   }
